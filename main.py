295ba14901671e147c0d9d40d4f5210a9ddba7a1
UserId56 <zaregay_menay@mail.ru>
2022-03-12 17:12:24 +0700

    complited_dz_2

--- main.py
+++ main.py
@@ -69,7 +69,51 @@ print(type(15 / 3))
 print(type(15 // 2))
 print(type(15 ** 2))
 
-#______________________________задание 2___________________________
+#______________________________задание 2 and 3___________________________
 l = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+is_final = True
+i = 0
+result = ''
+while is_final:
+    if not l[i].isalpha():
+        if int(l[i]) // 10 == 0:
+            l_width = 2 if len(l[i]) == 1 else 3
+            l[i] = l[i].zfill(l_width)
+        l = l[0:i] + ['"', l[i], '"'] + l[i+1:len(l)]
+        i += 3
+    else:
+        i += 1
+    if i == len(l):
+        is_final = False
+is_first = True
 for item in l:
-    print("ЗАЛУПА НИХУЯ НЕ ПОНЯТНО")
+    if item != '"' and item.isalpha():
+        result += item + " "
+    else:
+        result += item if is_first or (not item.isalpha() and item != '"') else item + " "
+        is_first = not is_first if item == '"' else is_first
+print(result)
+#______________________________задание 4___________________________
+user_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+for user_data in user_list:
+    user_name = user_data.split()[-1].title()
+    print(f'Привет, {user_name}')
+#______________________________задание 5___________________________
+
+price = [56.1, 32, 31.7, 127, 432.1, 670, 35, 76.5, 1, 2.7]
+result_str = ''
+for item_price in price:
+    temp_list_price = str(item_price).split('.')
+    if len(temp_list_price) < 2:
+        rub = temp_list_price[0] + " руб " if len(temp_list_price[0]) > 1 else temp_list_price[0].zfill(2) + " руб "
+        result_str += rub + "00 коп, "
+    else:
+        rub = temp_list_price[0] + " руб " if len(temp_list_price[0]) > 1 else temp_list_price[0].zfill(2) + " руб "
+        cop = temp_list_price[1] + " коп, " if len(temp_list_price[1]) > 1 else temp_list_price[1].zfill(2) + " коп, "
+        result_str += rub + cop
+print(result_str)
+print(sorted(price))
+print(price)
+sort_price = sorted(price, reverse=True)
+print(sort_price)
+print(sorted(sort_price[0:5]))
