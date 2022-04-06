cbfd4d48e6e825aebc2220993db3cd4aa72eb9e1
UserId56 <zaregay_menay@mail.ru>
2022-04-07 01:06:12 +0700

    dz complited

--- lesson_5.py
+++ lesson_5.py
@@ -1,8 +1,55 @@
-def get_num(nums):
-    for num in nums:
-        if num % 2 != 0:
-            yield num
+def get_duct(tutors, klasses):
+    for i in range(len(tutors)):
+        yield (tutors[i], klasses[i] if i<len(klasses) else None)
 
-generator_num = get_num(6)
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+klasses2 = [
+    '9А', '7В'
+]
+generator = get_duct(tutors, klasses2)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
 
-print(next(generator_num))
+#---------------задние 4 ----------------------
+def get_num_list(num_list):
+    for num in num_list:
+        yield num
+
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+tem_prev_num = src[0]
+generator_num = get_num_list(src)
+result = []
+
+for i in range(1, len(src)):
+    if src[i] > tem_prev_num:
+        result.append(src[i])
+    tem_prev_num = src[i]
+print(result)
+
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+
+def generator_src(src_list):
+    for item in src_list:
+        yield item
+
+generator_init = generator_src(src)
+result = []
+for i in range(len(src)):
+    item_search = next(generator_init)
+    if src.count(item_search) == 1:
+        result.append(item_search)
+
+print(result)
--- main.py
+++ main.py
@@ -1,75 +1,16 @@
-#---------------------------Задание 1--------------------------
-# duration = 400153
-# result = ''
-# Time_dictionary = [
-#     {
-#         'title': 'дней',
-#         'time': 86400
-#     },
-#     {
-#         'title': 'часов',
-#         'time': 3600
-#     },
-#     {
-#         'title': 'минут',
-#         'time': 60
-#     },
-#     {
-#         'title': 'секунд',
-#         'time': 1
-#     }
-# ]
-# for item in Time_dictionary:
-#     temp_duration = duration // item['time']
-#     if temp_duration != 0:
-#         result = f'{result} {str(temp_duration)} {item["title"]}'
-#         duration = duration % item['time']
-# print(result)
-#
-# #---------------------------Задание 2--------------------------
-# list_number = []
-# sum_numbers_seven = 0
-# for number_item in range(1, 1001, 2):
-#     coub_number = number_item**3
-#     list_number.append(coub_number)
-#     temp_summ = 0
-#     for number_element in str(coub_number):
-#         temp_summ += int(number_element)
-#     if temp_summ % 7 == 0:
-#         sum_numbers_seven += coub_number
-# print(sum_numbers_seven)
-# print(list_number)
-# sum_numbers_seven = 0
-# for i in range(0, len(list_number)):
-#     list_number[i] += 17
-#     temp_summ = 0
-#     for number_element in str(list_number[i]):
-#         temp_summ += int(number_element)
-#     if temp_summ % 7 == 0:
-#         sum_numbers_seven += list_number[i]
-# print(sum_numbers_seven)
-# print(list_number)
-# #---------------------------Задание 3--------------------------
-# for i in range(1, 100):
-#     number_ten = i // 10
-#     number_last = i % 10
-#     if number_ten == 1:
-#         print(i, ' процентов')
-#     elif number_last == 1:
-#         print(i, ' процент')
-#     elif number_last == 2 or number_last == 3 or number_last == 4:
-#         print(i, ' процента')
-#     else:
-#         print(i, ' процентов')
-#_______Lesson 2________________
-#______________________________задание 1___________________________
+# This is a sample Python script.
 
-print(type(15 * 3))
-print(type(15 / 3))
-print(type(15 // 2))
-print(type(15 ** 2))
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-#______________________________задание 2___________________________
-l = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-for item in l:
-    print("ЗАЛУПА НИХУЯ НЕ ПОНЯТНО")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
